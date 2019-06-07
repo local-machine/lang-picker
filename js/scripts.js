af6b910d58fb684b8e6a5d5c0794ce4eb786ab19
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-07 12:22:34 -0700

    Made changes to JS file and tested

--- js/scripts.js
+++ js/scripts.js
@@ -8,37 +8,37 @@ $(document).ready(function() {
     event.preventDefault();
 
     if (build1 === "Game"){
-      var num1 = 2
-    } else if (build1 === "Web"){
       var num1 = 3
+    } else if (build1 === "Web"){
+      var num1 = 6
     } else if (build1 === "Mobile"){
-      var num1 = 5
+      var num1 = 9
     } else if (build1 === "Desktop Software"){
-      var num1 = 7
+      var num1 = 12
     } else {
-      var num1 = 11
+      var num1 = 15
     }
 
     if (platform1 === "Windows"){
-      var num2 = 13;
+      var num2 = 18;
     } else if (platform1 === "Mac"){
-      var num2 = 17;
+      var num2 = 21;
     } else if (platform1 === "iOS"){
-      var num2 = 19;
+      var num2 = 24;
     } else {
-      var num2 = 23;
+      var num2 = 27;
     }
 
     if (interest1 === "How to interact with things"){
-      var num3 = 29
+      var num3 = 30
     } else {
-      var num3 = 31
+      var num3 = 33
     }
 
       var result = (num1*num2*num3)%3
 
       prompt("hi");
-      $("#celphoto").append('<img src=img/celeb' + result +'.jpg>');
+      $("#lang").append('<img src=img/lang' + result +'.jpg>');
 
 
       // event.preventDefault();
