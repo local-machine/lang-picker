cc487cce918bf6a7b8b4e553c856cd53d502dbf8
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-07 16:02:42 -0700

    Tried to get my function to be operational

--- js/scripts.js
+++ js/scripts.js
@@ -9,41 +9,41 @@ $(document).ready(function() {
     event.preventDefault();
 
     if (build1 === "Game"){
-      var num1 = 3
+      var num1 = 1
     } else if (build1 === "Web"){
-      var num1 = 6
+      var num1 = 2
     } else if (build1 === "Mobile"){
-      var num1 = 9
+      var num1 = 3
     } else if (build1 === "Desktop Software"){
-      var num1 = 12
+      var num1 = 4
     } else {
-      var num1 = 15
+      var num1 = 5
     }
 
     if (platform1 === "Windows"){
-      var num2 = 18;
+      var num2 = 1;
     } else if (platform1 === "Mac"){
-      var num2 = 21;
+      var num2 = 2;
     } else if (platform1 === "iOS"){
-      var num2 = 24;
+      var num2 = 3;
     } else {
-      var num2 = 27;
+      var num2 = 4;
     }
 
     if (interest1 === "How to interact with things"){
-      var num3 = 30
+      var num3 = 1
     } else {
-      var num3 = 33
+      var num3 = 2
     }
     if (animal1 === "Dog"){
-      var num4 = 30
+      var num4 = 1
     } else {
-      var num4 = 33
+      var num4 = 2
     }
     if (important1 === "Obtaining a job"){
-      var num5 = 30
+      var num5 = 1
     } else {
-      var num5 = 33
+      var num5 = 2
     }
     prompt("hi");
       //return Math.floor(Math.random() * 4);
