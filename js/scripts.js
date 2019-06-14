afc8258b0c42e614d26083af963ac0801bacec67
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-13 21:54:07 -0700

    Changed questions and function

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,61 @@
 $(document).ready(function() {
   $("form#questionnaire").submit(function(event) {
-    var build1 = ($("#build").val());
-    var platform1 = ($("#platform").val());
-    var interest1 = ($("#interest").val());
-    var animal1 = ($("#animal").val());
-    var important1 = ($("#important").val());
+    var country = ($("#country").val());
+    var lotr = ($("#lotr").val());
+    var weapon = ($("#weapon").val());
+    var money = ($("#money").val());
+    var admire = ($("#admire").val());
 
     event.preventDefault();
 
-    if (build1 === "Game"){
-      var num1 = 1
-    } else if (build1 === "Web"){
-      var num1 = 2
-    } else if (build1 === "Mobile"){
-      var num1 = 3
-    } else if (build1 === "Desktop Software"){
-      var num1 = 4
+    if (country === "Italy - Massively influential and loved by everyone, but breaks down easily."){
+      var quest1 = 1
+    } else if (country === "Netherlands - Hey no problem, let'sh do it guysh!"){
+      var quest1 = 2
+    } else if (country === "Switzerland -  Beautiful and well thought-out, but expect to pay a lot if you want to get seriously involved."){
+      var quest1 = 3
     } else {
-      var num1 = 5
+      var quest1 = 4
     }
 
-    if (platform1 === "Windows"){
-      var num2 = 1;
-    } else if (platform1 === "Mac"){
-      var num2 = 2;
-    } else if (platform1 === "iOS"){
-      var num2 = 3;
+    if (lotr === "Hobbit"){
+      var quest2 = 1;
+    } else if (lotr === "The Ent"){
+      var quest2 = 2;
+    } else if (lotr === "Elf"){
+      var quest2 = 3;
     } else {
-      var num2 = 4;
+      var quest2 = 4;
     }
 
-    if (interest1 === "How to interact with things"){
-      var num3 = 1
+    if (weapon === "A sword without a hilt"){
+      var quest3 = 1
+    } else if (weapon === "A “v2/v3” double barrel shotgun"){
+      var quest3 = 2
+    } else if (weapon === "A laser rifle strapped to a donkey"){
+      var quest3 = 3;
     } else {
-      var num3 = 2
+      var quest3 = 4;
     }
-    if (animal1 === "Dog"){
-      var num4 = 1
+    if (money === "I want to be comfortable."){
+      var quest4 = 1
+    } else if (money === "A lot."){
+      var quest4 = 2
+    } else if (money === "I don't care."){
+      var quest4 = 3;
     } else {
-      var num4 = 2
+      var quest4 = 4
     }
-    if (important1 === "Obtaining a job"){
-      var num5 = 1
+    if (admire === "Paypal, front-end of websites"){
+      var quest5 = 1
+    } else if (admire === "YouTube, Instagram & Spotify"){
+      var quest5 = 2
+    } else if (admire === "Enterprise & Windows applications"){
+      var quest5 = 3;
     } else {
-      var num5 = 2
+      var quest5 = 4
     }
+
     //prompt("hi");
       //return Math.floor(Math.random() * 4);
       var result = (num1+num2+num3+num4+num5)%5
